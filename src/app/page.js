70f172b0fdf68717def05024fc052fb796bc0a1c
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-10-08 18:07:21 +0600

    added popular courses

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Newsletter from "@/components/Newsletter";
 import TopCategories from "@/components/TopCategories";
+import PopularCourses from "@/components/PopularCourses";
 
 
 export default function Home() {
   return (
    <div>
     <TopCategories></TopCategories>
+    <PopularCourses></PopularCourses>
     <Newsletter></Newsletter>
    </div>
   );
---
+++ src/components/PopularCourses.jsx
@@ -0,0 +1,73 @@
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { MdEventNote } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+
+const PopularCourses = () => {
+    return (
+        <div className="max-w-[1440px] my-10 w-10/12 mx-auto">
+            <div className="flex justify-between">
+                <h1 className="text-4xl font-bold">Popular Courses</h1>
+                <Link  className="underline" href="/ViewAll">View All</Link>
+            </div>
+            <div className="mt-10 grid gap-10 grid-cols-1 lg:grid-cols-3">
+                <div className="space-y-5 ">
+                <Image width="400"  height="200" src="/assets/Rectangle 2.png" alt="logo" /> 
+                <p className="bg-[#F3F2FF] inline-block px-5 py-2 rounded-md">Design</p>
+                <h4 className="font-bold">The 6 Steps to Building a 
+                powerful Personal brand</h4>
+                <p calssName="flex"><MdEventNote />
+                <span>2 Lesoons</span></p>
+                <div className="flex justify-between" >
+                    <p calssName="flex"><FaUsers />
+                   <span> 30 Students
+                    enrolled</span></p>
+                    <button className="btn btn-outline">View Course</button>
+                </div>
+                </div>
+                <div className="space-y-5 ">
+                <Image width="400"  height="200" src="/assets/Rectangle 2.png" alt="logo" /> 
+                <p className="bg-[#F3F2FF] inline-block px-5 py-2 rounded-md">Design</p>
+                <h4 className="font-bold">The 6 Steps to Building a 
+                powerful Personal brand</h4>
+                <p calssName="flex"><MdEventNote />
+                <span>2 Lesoons</span></p>
+                <div className="flex justify-between" >
+                    <p calssName="flex"><FaUsers />
+                   <span> 30 Students
+                    enrolled</span></p>
+                    <button className="btn btn-outline">View Course</button>
+                </div>
+                </div>
+                <div className="space-y-5 ">
+                <Image width="400"  height="200" src="/assets/Rectangle 2.png" alt="logo" /> 
+                <p className="bg-[#F3F2FF] inline-block px-5 py-2 rounded-md">Design</p>
+                <h4 className="font-bold">The 6 Steps to Building a 
+                powerful Personal brand</h4>
+                <p calssName="flex"><MdEventNote />
+                <span>2 Lesoons</span></p>
+                <div className="flex justify-between" >
+                    <p calssName="flex"><FaUsers />
+                   <span> 30 Students
+                    enrolled</span></p>
+                    <button className="btn btn-outline">View Course</button>
+                </div>
+                </div>
+            </div>
+            <div className="flex mt-10 justify-center">
+            <div className="join">
+  <button className="join-item btn">{"<"}</button>
+  <button className="join-item btn">1</button>
+  <button className="join-item btn">2</button>
+  <button className="join-item btn btn-disabled">...</button>
+  <button className="join-item btn">99</button>
+  <button className="join-item btn">100</button>
+  <button className="join-item btn">{">"}</button>
+</div>
+            </div>
+        </div>
+    );
+};
+
+export default PopularCourses;
